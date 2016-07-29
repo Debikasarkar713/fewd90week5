e6a412d89c3a379b716b5fbec7508a5ed795861e
Hart Liddell <hart@leapingfish.io>
2016-07-29 10:55:23 -0400

    Hart finishing off some functionality

--- js/project.js
+++ js/project.js
@@ -5,23 +5,25 @@ document.getElementById("name").onfocus = clearName;
 document.getElementById("grade").onfocus = clearGrade;
 
 function clearName() {
-  document.getElementById("name").value = "";
+	document.getElementById("name").value = "";
 }
+
 function clearGrade() {
-  document.getElementById("grade").value = "";
+	document.getElementById("grade").value = "";
 }
+
 function showRoster() {
-document.querySelector(".roster").classList.toggle("open");
-document.querySelector("#roster").classList.toggle("selected");
-document.querySelector(".assign").classList.remove("open");
-document.querySelector("#assign").classList.remove("selected");
+	document.querySelector(".roster").classList.toggle("open");
+	document.querySelector("#roster").classList.toggle("selected");
+	document.querySelector(".assign").classList.remove("open");
+	document.querySelector("#assign").classList.remove("selected");
 }
 document.getElementById("roster").onclick = showRoster;
 
 function showInput() {
-document.querySelector(".roster").classList.toggle("open");
-document.querySelector("#roster").classList.toggle("selected");
-document.querySelector(".assign").classList.remove("open");
-document.querySelector("#assign").classList.remove("selected");
+	document.querySelector(".assign").classList.toggle("open");
+	document.querySelector("#assign").classList.toggle("selected");
+	document.querySelector(".roster").classList.remove("open");
+	document.querySelector("#roster").classList.remove("selected");
 }
 document.getElementById("assign").onclick = showInput;
